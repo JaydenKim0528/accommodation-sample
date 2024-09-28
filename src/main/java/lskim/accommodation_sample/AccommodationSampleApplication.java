42bbc9e53e34d2022e84ef2a50785d4953311dd1
jayden <alien01918282@gmail.com>
2024-09-28 13:17:54 +0900

    Initial commit

---
+++ src/main/java/lskim/accommodation_sample/AccommodationSampleApplication.java
@@ -0,0 +1,13 @@
+package lskim.accommodation_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lskim/accommodation_sample/AccommodationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lskim.accommodation_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
