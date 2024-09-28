0692d6cc3b7b7485f34547d0539f3d32d6133cac
jayden <alien01918282@gmail.com>
2024-09-28 14:03:31 +0900

    feat : Project Setting

---
+++ src/main/java/lskim/accommodation_sample/accommodation/web/common/AccommodationRestController.java
@@ -0,0 +1,18 @@
+package lskim.accommodation_sample.accommodation.web.common;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AccommodationRestController {
+
+
+
+
+
+}
